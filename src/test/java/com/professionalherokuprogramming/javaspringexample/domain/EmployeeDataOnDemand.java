37dd0892988212f44136002583941e9716d97c98
Marian Lucia Solis Ceciliano <malusoce96@gmail.com>
2014-11-19 20:47:22 -0600

    .war
    Prueba

--- src/test/java/com/professionalherokuprogramming/javaspringexample/domain/EmployeeDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package com.professionalherokuprogramming.javaspringexample.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Employee.class)
-public class EmployeeDataOnDemand {
-}
